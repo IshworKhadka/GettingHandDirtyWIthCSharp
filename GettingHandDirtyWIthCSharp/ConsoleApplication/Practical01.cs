108a8a0cfc3f74f0fe4b399afb5f5c75c57d3da9
Ishwor Khadka <eshworkhadka@hotmail.com>
2020-05-17 03:02:26 +0930

    Practical01 added

---
+++ GettingHandDirtyWIthCSharp/ConsoleApplication/Practical01.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GettingHandDirtyWIthCSharp.ConsoleApplication
+{
+    class Practical01
+    {
+        public void OutputFormat()
+        {
+            //42 = 7 * 6 (answer 1)
+            //3 + 5 = 8 (answer 2)
+            //The ideas of "state" and "sequence"
+            //are fundamental to most programming.
+
+            Console.WriteLine(42 + " = " + 7 + " * " + 6);
+            Console.WriteLine(3 + " + " + 5 + " = " + 8);
+            Console.WriteLine("The ideas of \"state\" and \"sequence\"");
+            Console.WriteLine("are funadmental to most programming.");
+        }
+
+    }
+}
--- GettingHandDirtyWIthCSharp/Program.cs
+++ GettingHandDirtyWIthCSharp/Program.cs
@@ -7,13 +7,15 @@ namespace GettingHandDirtyWIthCSharp
     {
         static void Main(string[] args)
         {
-            DrillExercises08 de = new DrillExercises08();
+            Practical01 pr1 = new Practical01();
+            pr1.OutputFormat();
+            //DrillExercises08 de = new DrillExercises08();
             //test git push
             //int[] array = { 1, 9, 9, 3, 9 };
             //Console.Write(de.ConvertToDecimal("01101011"));
             //Console.WriteLine(de.arrayOneTwoThree(array));
             //Console.WriteLine(de.arrayCountNines(array));
-            de.Product();
+            //de.Product();
         }
     }
 }
